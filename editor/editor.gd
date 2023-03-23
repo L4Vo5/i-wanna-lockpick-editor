927ecee9d82c14215c3c95daf8ec9b4cb118187e
L4Vo5 <l4vo5finalboss@gmail.com>
2023-03-23 02:22:59 -0300

    hhhhhh

--- editor/editor.gd
+++ editor/editor.gd
@@ -1,7 +1,13 @@
 extends Control
 
+@onready var tile_map: TileMap = %TileMap
+
 func _enter_tree() -> void:
 	Global.in_level_editor = true
 
 func _exit_tree() -> void:
 	Global.in_level_editor = false
+
+func _ready() -> void:
+	var l = 0
+	tile_map.set_cell(l, Vector2i(4,1), 1, Vector2i(0, 0))
