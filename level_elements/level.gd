59372adfa668af62f34cd3a4259ebe3a515284b4
Rob23oba <robin.arnez@web.de>
2024-06-15 08:25:14 +0200

    Refix mouseover going offscreen staying bug
    This reverts commit 5f2cf9b566427593ab972812c364018b40ba4b3f.

--- level_elements/level.gd
+++ level_elements/level.gd
@@ -124,6 +124,13 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	adjust_camera()
+	
+	var mouse_pos = get_local_mouse_position()
+	var camera_pos = camera.position
+	var camera_pos2 = camera_pos + camera.get_viewport_rect().size
+	if mouse_pos.x < camera_pos.x or mouse_pos.y < camera_pos.y \
+		or mouse_pos.x >= camera_pos2.x or mouse_pos.y >= camera_pos2.y:
+		hover_highlight.stop_adapting()
 
 # For legal reasons this should happen in a deferred call, so it's at the end of the frame and everything that happens in this frame had time to record their undo calls
 func undo() -> void:
