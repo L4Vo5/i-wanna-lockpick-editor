4548718028afc918382bc462cb234ca6073a1385
L4Vo5 <l4vo5finalboss@gmail.com>
2024-06-25 18:28:13 -0300

    oops fix test. vector2 comparisons do not work as I expected

--- tests/editor/side_editors/door_editor/lock_editor_test.gd
+++ tests/editor/side_editors/door_editor/lock_editor_test.gd
@@ -13,18 +13,26 @@ const LOCK_EDITOR = preload("res://editor/side_editors/door_editor/lock_editor.t
 func test_lock_size_bug() -> void:
 	var editor: LockEditor = LOCK_EDITOR.instantiate()
 	var lock_data := LockData.new()
+	lock_data.size = Vector2i(18, 18)
 	lock_data.color = Enums.colors.white
 	editor.lock_data = lock_data
 	add_child(editor)
 	# This is just to try and catch when the scene changes, because it means the test might need to be updated accordingly.
-	assert_object(editor.lock.get_parent()).is_instanceof(CenterContainer)
+	var lock_parent := editor.lock.get_parent()
+	assert_object(lock_parent).is_instanceof(CenterContainer)
 	
-	assert_vector(editor.lock.get_parent().size)\
-		.is_greater_equal(Vector2(lock_data.size))
-	# We neither notify the lock that it should change or wait a frame - again, adjust the test if the way things work changes
+	# Wait a frame to let all the containers position their children
+	# Technically unnecessary, but with this we check against the actual layout I guess
+	await await_idle_frame()
+	
+	# We neither notify the lock that it should change or wait a frame for the lock to catch up - again, adjust the test if the way things work changes
+	assert_float(lock_parent.size.x).is_greater_equal(18)
+	assert_float(lock_parent.size.y).is_greater_equal(18)
 	lock_data.size.y = 180
-	assert_vector(editor.lock.get_parent().size)\
-		.is_greater_equal(Vector2(lock_data.size))
-	
+	assert_float(lock_parent.size.x).is_greater_equal(18)
+	assert_float(lock_parent.size.y).is_greater_equal(180)
+	lock_data.size.x = 400
+	assert_float(lock_parent.size.x).is_greater_equal(400)
+	assert_float(lock_parent.size.y).is_greater_equal(180)
 	
 
