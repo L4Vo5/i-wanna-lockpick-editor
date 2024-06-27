5ec9c864769abfa8e94bc9a315488b573ff09d24
L4Vo5 <l4vo5finalboss@gmail.com>
2024-06-27 01:41:30 -0300

    rename editor_settings to settings + add sound volume slider

--- editor/side_editors/settings_editor/settings_editor.gd
+++ editor/side_editors/settings_editor/settings_editor.gd
@@ -1,11 +1,55 @@
 extends MarginContainer
 
 @onready var save_on_play: CheckBox = %SaveOnPlay
+@onready var sound_slider: HSlider = %SoundSlider
+@onready var cacophony: CheckButton = %Cacophony
 
+var all_sounds: Array[AudioStream] = []
 func _ready() -> void:
-	save_on_play.button_pressed = Global.editor_settings.should_save_on_play
+	set_to_current_settings()
 	save_on_play.toggled.connect(_on_save_on_play_toggled)
 	save_on_play.tooltip_text = "Save the level automatically before playing it"
+	sound_slider.value_changed.connect(_on_sound_slider_value_changed)
+	cacophony.toggled.connect(_on_cacophony_toggled)
+
+
+func set_to_current_settings() -> void:
+	save_on_play.button_pressed = Global.editor_settings.should_save_on_play
+	sound_slider.value = Global.editor_settings.sound_volume
 
 func _on_save_on_play_toggled(is_toggled: bool) -> void:
 	Global.editor_settings.should_save_on_play = is_toggled
+
+func _on_sound_slider_value_changed(value: float) -> void:
+	Global.editor_settings.sound_volume = value
+
+
+func _on_cacophony_toggled(toggled_on: bool) -> void:
+	if not toggled_on:
+		for child in cacophony.get_children():
+			cacophony.remove_child(child)
+	else:
+		if all_sounds.is_empty():
+			load_all_sounds()
+		assert(AudioServer.get_bus_index(&"Sfx") != -1)
+		for _i in 8:
+			var node := AudioStreamPlayer.new()
+			node.bus = &"Sfx"
+			cacophony.add_child(node)
+			node.stream = all_sounds[randi() % all_sounds.size()]
+			node.play()
+			node.finished.connect(func():
+				node.stream = all_sounds[randi() % all_sounds.size()]
+				node.play()
+			)
+
+func load_all_sounds() -> void:
+	var dirs := ["res://"]
+	while not dirs.is_empty():
+		var dir: String = dirs.pop_back() as String
+		for new_dir in DirAccess.get_directories_at(dir):
+			dirs.push_back(dir.path_join(new_dir))
+		for file_name in DirAccess.get_files_at(dir):
+			if file_name.get_extension() == "wav":
+				var sound = load(dir.path_join(file_name))
+				all_sounds.push_back(sound)
--- misc/editor_settings.gd
+++ misc/editor_settings.gd
@@ -7,6 +7,16 @@ var should_save_on_play := true:
 		_config_file.set_value("", "ShouldSaveOnPlay", val)
 		save()
 
+var sound_volume := 1.0:
+	set(val):
+		sound_volume = val
+		var idx := AudioServer.get_bus_index(&"Sfx")
+		assert(idx != -1)
+		var db := linear_to_db(val)
+		AudioServer.set_bus_volume_db(idx, db)
+		_config_file.set_value("", "SoundVolume", val)
+		save()
+
 const PATH := "user://editor_settings.cfg"
 var _config_file := ConfigFile.new()
 
@@ -15,6 +25,7 @@ func _init() -> void:
 	if err != OK:
 		return
 	should_save_on_play = _config_file.get_value("", "ShouldSaveOnPlay", true)
+	sound_volume = _config_file.get_value("", "SoundVolume", 1.0)
 
 func save() -> void:
 	_config_file.save(PATH)
