4a5b88f3abe7f71ab3cc35c0dae4081d8de98263
L4Vo5 <l4vo5finalboss@gmail.com>
2024-07-14 19:52:15 -0300

    added level icon

--- editor/bookmark_tab_container/bookmark_tab_container.gd
+++ editor/bookmark_tab_container/bookmark_tab_container.gd
@@ -18,7 +18,7 @@ const name_to_icon := {
 	SalvagePoints = preload("res://editor/bookmark_tab_container/icons/salvage_point.png"),
 	Entries = preload("res://editor/bookmark_tab_container/icons/entry.png"),
 	Level = preload("res://editor/bookmark_tab_container/icons/level.png"),
-	LevelPack = preload("res://editor/bookmark_tab_container/icons/level.png"), # TODO: differenciate
+	LevelPack = preload("res://editor/bookmark_tab_container/icons/level_pack.png"),
 	Settings = preload("res://editor/bookmark_tab_container/icons/settings.svg"),
 	MultipleSelection = preload("res://editor/bookmark_tab_container/icons/multi_selection.svg"),
 }
